dbecaef6b118b2faff15464c36ec5e0a900b3c2f
Widi Widayat <widayatwidi26@gmail.com>
2022-12-08 06:40:09 +0700

    first App test done

--- testapp/lib/main.dart
+++ testapp/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    final words = WordPair.random();
+    // final words = WordPair.random();
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
@@ -21,11 +21,43 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Welcome to FlutterDevDaily'),
         ),
-        body: Center(
-          child: Text(words.asPascalCase),
+        body: const Center(
+          child: RandomWords(),
         ),
       ),
     );
   }
 }
 
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({Key? key}) : super(key: key);
+
+  @override
+  State<RandomWords> createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18);
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        if(i.isOdd) return const Divider();
+        final index = i ~/ 2;
+        if(index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        // return Text(_suggestions[index].asPascalCase);
+        return ListTile(
+          title: Text(
+            _suggestions[index].asPascalCase,
+            style: _biggerFont,
+          ),
+        );
+      },
+    );
+  }
+}
